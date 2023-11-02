dce910ca5f6c86fc2ce68ba59148e06d06690ec3
Mohammed Yaseen <mohammedysn15@gmail.com>
2023-11-02 12:01:29 +0000

    Add remove and keep commands

--- main.py
+++ main.py
@@ -30,20 +30,55 @@ def remove_pages(input_pdf, output_pdf, page_ranges_to_remove):
             pdf_writer.write(output_file)
 
 
+def keep_pages(input_pdf, output_pdf, page_ranges_to_keep):
+    print(
+        f"Keeping pages {page_ranges_to_keep} from {input_pdf} and saving to {output_pdf}"
+    )
+
+    pdf_writer = PyPDF2.PdfWriter()
+
+    with open(input_pdf, "rb") as pdf_file:
+        pdf_reader = PyPDF2.PdfReader(pdf_file)
+        total_pages = len(pdf_reader.pages)
+
+        pages_to_keep = []
+        for page_range in page_ranges_to_keep:
+            start, end = page_range
+            if 1 <= start <= end <= total_pages:
+                pages_to_keep.extend(range(start - 1, end))
+            else:
+                print(f"Invalid page range: {page_range}. Skipping.")
+
+        for page_num in range(total_pages):
+            if page_num in pages_to_keep:
+                page = pdf_reader.pages[page_num]
+                pdf_writer.add_page(page)
+
+        with open(output_pdf, "wb") as output_file:
+            pdf_writer.write(output_file)
+
+
 if __name__ == "__main__":
     input_pdf = "/mnt/c/Users/moham/Downloads/routing.pdf"
     output_pdf = "/mnt/c/Users/moham/Downloads/short_routing.pdf"
 
     parser = argparse.ArgumentParser(
-        description="Remove specific page ranges from a PDF."
+        description="Modify a PDF by removing or keeping specific page ranges."
     )
     parser.add_argument(
-        "page_ranges_to_remove",
+        "action",
+        choices=["remove", "keep"],
+        help="Specify whether to remove or keep specific page ranges.",
+    )
+    parser.add_argument(
+        "page_ranges",
         nargs="+",
         type=lambda x: tuple(map(int, x.split("-"))),
-        help="Page ranges to remove in the format 'start-end' (e.g., 1-10 20-30)",
+        help="Page ranges in the format 'start-end' (e.g., 1-10 20-30)",
     )
     args = parser.parse_args()
-    page_ranges_to_remove = args.page_ranges_to_remove
 
-    remove_pages(input_pdf, output_pdf, page_ranges_to_remove)
+    if args.action == "remove":
+        remove_pages(input_pdf, output_pdf, args.page_ranges)
+    elif args.action == "keep":
+        keep_pages(input_pdf, output_pdf, args.page_ranges)
